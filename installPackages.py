1a7a2cd842838323e54cf0e5e29ad885040f8980
Rvlife <32027061+Rvlis@users.noreply.github.com>
2020-12-22 10:50:13 +0800

    Delete installPackages.py

--- installPackages.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("conda install -c conda-forge tesserocr")
-os.system("pip install pdfminer.six")
-os.system("pip install docx")
-os.system("pip install pypiwin32")
-os.system("pip install bs4")
-os.system("pip install tencentcloud-sdk-python")
