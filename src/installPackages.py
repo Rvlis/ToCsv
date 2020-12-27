1add82dfdfc821313a36e27de0544b6fc25f749a
Rvlife <32027061+Rvlis@users.noreply.github.com>
2020-12-27 18:56:42 +0800

    Update installPackages.py

--- src/installPackages.py
+++ src/installPackages.py
@@ -2,7 +2,7 @@ import os
 
 os.system("conda install -c conda-forge tesserocr")
 os.system("pip install pdfminer.six")
-os.system("pip install docx")
+os.system("pip install python-docx")
 os.system("pip install pypiwin32")
 os.system("pip install bs4")
 os.system("pip install tencentcloud-sdk-python")
